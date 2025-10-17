3459dc0fa91115b6b68a4ab6fd532d6019636ed0
HenryKristofani <henry.kristofani@gmail.com>
2025-10-17 16:37:54 +0700

    minor

--- app/tournaments-lists.tsx
+++ app/tournaments-lists.tsx
@@ -59,7 +59,7 @@ export default function TournamentsListPage() {
 										href={`/tournaments/${tournament.id}/bracket`}
 										className="px-4 py-2 bg-green-700 text-white rounded hover:bg-green-800 transition"
 									>
-										Open
+										Open Bracket
 									</Link>
 								</li>
 							))
